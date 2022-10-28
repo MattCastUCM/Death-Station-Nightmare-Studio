7f1439f289f21bc2d3d11913eb7b27eab0ffe5d6
MattCastUCM <mattcast@ucm.es>
2022-10-28 18:57:49 +0200

    Refactorización

--- src/objetos/character.js
+++ src/objetos/character.js
@@ -1,11 +1,12 @@
-// Clase para las paredes (y algunos elementos del escenario).
-// es de tipo Sprite pero no tiene una imagen asociada.
-// Tendrá un collider para que no se puedan atravesar.
+// Clase para los personajes (jugador, enemigos y gato
  export default class Wall extends Phaser.GameObjects.Sprite {
 	// Constructora que recibe la escena en la que se va a crear,
-	// sus coordenadas, y sus dimensiones
-    constructor(scene, posX, posY, w, h) {
-      super(scene, posX, posY);
+	// sus coordenadas, su escala, su textura, y su velocidad
+    constructor(scene, posX, posY, scale, texture, spd) {
+      super(scene, posX, posY, texture);
+	  this.speed = spd;
+	  
+	  
       this.scene.add.existing(this);
 	  
 	  // Añade físicas y un collider a la pared
--- src/script.js
+++ src/script.js
@@ -1,12 +1,3 @@
-function devuelveTextoDeAlerta() {
-  return "uooooo! Vaya alerta";
-}
-
-function desaparece(nombre) {
-	var button = document.getElementById(nombre);
-  button.style.visibility='hidden';
-}
-
 function ScrollToTop(){
   window.scrollTo({
     top: 0
