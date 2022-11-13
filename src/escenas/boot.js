d163b7c84ff9cec68cc44d5fab12bc4e3e7a8fdd
nanxiqin <nanxiqin@ucm.es>
2022-11-13 22:13:04 +0100

    Implementado inventario y modificado player y boot

--- src/escenas/boot.js
+++ src/escenas/boot.js
@@ -26,7 +26,22 @@ export default class Boot extends Phaser.Scene {
 			'src/objetos/Cat.js',
 
 	   ]);
+
+	   //HUD
+	   this.load.image('heartImg', 'assets/HUD/corazon.png');
+	   this.load.image('inventory', 'assets/HUD/inventario.png');
+	   this.load.image('pausa', 'assets/HUD/pausa.png');
+	   this.load.image('level', 'assets/HUD/level.png');
+	   this.load.image('play', 'assets/HUD/play.png');
+	   this.load.image('selected', 'assets/HUD/seleccionado.png');
+
+	   //armas
+	 this.load.spritesheet('navaja', 'assets/Armas/Cuchillo.png', { frameWidth: 210, frameHeight: 480});
+	 this.load.spritesheet('botella', 'assets/Armas/Botella.png', { frameWidth: 140, frameHeight: 380});
+	 this.load.spritesheet('barra', 'assets/Armas/Barra.png', { frameWidth: 32, frameHeight: 48 });
+	 this.load.spritesheet('hacha', 'assets/Armas/Hacha.png', { frameWidth: 100, frameHeight: 220 });
 	}
+
 	
 	create() {
 		this.scene.start('menu');
